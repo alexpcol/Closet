869bb6c15fbf92decd3785b25cc068acc2167dee
Alex <alexpc94@gmail.com>
2019-06-08 09:49:23 -0500

    feat: added dummy test for expectations

--- ClosetTests/ClosetTests.swift
+++ ClosetTests/ClosetTests.swift
@@ -11,17 +11,35 @@ import XCTest
 
 class ClosetTests: XCTestCase {
 
+    var sut: URLSession!
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        super.tearDown()
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        let promise = expectation(description: "Status code: 200")
+        let dataTask = sut.dataTask(with: url!) { (data, response, error) in
+            if let error = error {
+                XCTFail("Error \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
     }
 
     func testPerformanceExample() {
